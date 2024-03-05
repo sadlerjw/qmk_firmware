59db75aaa55178941b73b96d6fefe47bcbacf9f8
Jason Sadler <jason@sadlerjw.com>
2024-03-05 02:39:11 -0500

    Try to do status-based LEDs

--- keyboards/crkbd/keymaps/json/keymap.c
+++ keyboards/crkbd/keymaps/json/keymap.c
@@ -1,4 +1,4 @@
-/*
+    /*
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
 
@@ -68,3 +68,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+#ifdef RGB_MATRIX_ENABLE
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+
+        HSV hsv = { 120, 255, rgb_matrix_config.hsv.v };
+        RGB rgb = hsv_to_rgb(hsv);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index < led_max && index != NO_LED) {
+                    if (keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                        rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.g);
+                    } else {
+                        rgb_matrix_set_color(index, RGB_OFF);
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+#endif
